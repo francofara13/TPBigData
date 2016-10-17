461c5a0074b5edf32948ec23cd0fe77778052e54
Agustin Toledo <toldo@skynet.skynet.com>
2016-10-17 19:09:19 -0300

    update

---
+++ db.sql
@@ -0,0 +1,66 @@
+// Facturacion y ventas
+
+CREATE TABLE Departamentos (
+    Id_depto serial primary key,
+    Nom_depto int NULL,
+    Zona varchar NULL
+)
+
+CREATE TABLE Ciudades (
+    Id_ciudad serial primary key,
+    Id_depto int not null, 
+    Nom_ciudad varchar(50) not null, 
+    Poblacion int not null,
+    Clasificacion varchar(50),
+)
+
+CREATE TABLE Rubros (
+    Id_rubro serial primary key,
+    Nom_rubro varchar(50) not NULL
+)
+
+CREATE TABLE Clientes (Id_cliente serial primary key,
+    Nombre varchar(50) not null,
+    Direccion varchar(50) not null,
+    Telefono varchar(50) not null,
+    Ciudad int references Ciudades(Id_ciudad), /*FK*/
+    Departamento references Departamentoreferences(Id_depto),/*FK*/
+    Rubro varchar(50) not null,
+    Categoria varchar(50) not null,
+    Fecha alta date not null
+ )
+
+CREATE TABLE Facturas (Factura serial primary key,
+     Fecha date not null,
+     Cliente int references Clientes(Id_cliente), /*FK*/
+     Vendedor int references Vendedores(Id_vendedor) /*FK*/
+)
+CREATE TABLE Registros-Facturas (Factura serial primary key,
+     Articulo int not null, /*double pk*/
+     Importe int not null,
+     Unidades int not null)
+
+ALTER TABLE Registros-Facturas ADD CONSTRAINT "ID_reg_fact" PRIMARY KEY (Facturacion, Articulo);
+
+
+CREATE TABLE Articulos (Id_articulo serial primary key,
+     Id_producto int references Productos(Id_producto),/*FK*/
+     Id_tamaño int not null)
+
+CREATE TABLE Productos (Id_producto serial primary key,
+    Id_familia int not null,
+    Id_duracion int not null)
+
+CREATE TABLE Codigos (Tipo serial primary key,
+    Codigo int not null, /*double pk*/
+    Descripcion varchar(50) not null) 
+
+ALTER TABLE Codigos ADD CONSTRAINT "ID_codigos" PRIMARY KEY (Tipo, Codigo);
+
+
+CREATE TABLE Vendedores (Id_vendedor serial primary key,
+     Nombre varchar(50) not null,
+     Direccion varchar(50) not null,
+     Telefono varchar(50) not null,
+     Especialidad varchar(50) not null
+) 
